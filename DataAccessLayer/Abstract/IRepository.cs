5beae5bbdc5c7dce1ea6fc4cde92221eee4c0f3b
Ahmet Osman Sezgin <180401022@ogr.comu.edu.tr>
2021-04-30 23:20:43 +0300

    20. Ders Sonu

--- DataAccessLayer/Abstract/IRepository.cs
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,5 +13,7 @@ namespace DataAccessLayer.Abstract
         void Insert(T t);
         void Update(T t);
         void Delete(T t);
+
+        List<T> List(Expression<Func<T, bool>> filter);
     }
 }
--- DataAccessLayer/Concrete/Repositories/WriterRepository.cs
+++ DataAccessLayer/Concrete/Repositories/WriterRepository.cs
@@ -10,37 +10,34 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Concrete.Repositories
 {
-    public class CategoryRepository : ICategoryDal
+    public class WriterRepository : IWriterDal
     {
         Context context = new Context();
         DbSet<Category> _object;
 
-
-        public void Delete(Category c)
-        {
-            _object.Remove(c);
-            context.SaveChanges();
-        }
-
-        public void Insert(Category c)
-        {
-            _object.Add(c);
-            context.SaveChanges();
-        }
-
-        public List<Category> List()
-        {
-            return _object.ToList();
-        }
-
-        public List<Category> List(Expression<Func<Category, bool>> filter)
+        public void Delete(Writer t)
         {
             throw new NotImplementedException();
         }
 
-        public void Update(Category c)
+        public void Insert(Writer t)
         {
-            context.SaveChanges();
+            throw new NotImplementedException();
+        }
+
+        public List<Writer> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Writer> List(Expression<Func<Writer, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Writer t)
+        {
+            throw new NotImplementedException();
         }
     }
 }
