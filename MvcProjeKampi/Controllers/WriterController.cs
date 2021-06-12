9873c89c13b6e0d89b4dc28e6ed3322268771375
Ahmet Osman Sezgin <180401022@ogr.comu.edu.tr>
2021-06-12 18:36:06 +0300

    45. Ders Sonu

--- MvcProjeKampi/Controllers/WriterController.cs
+++ MvcProjeKampi/Controllers/WriterController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,11 +14,67 @@ namespace MvcProjeKampi.Controllers
     public class WriterController : Controller
     {
         WriterManager writerManager = new WriterManager(new EfWriterDal());
+        WriterValidator writerValidator = new WriterValidator();
+
         // GET: Writer
         public ActionResult Index()
         {
             var WriterValues = writerManager.GetList();
             return View(WriterValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddWriter()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddWriter(Writer w)
+        {
+            ValidationResult results = writerValidator.Validate(w);
+            if (results.IsValid)
+            {
+                writerManager.WriterAdd(w);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult EditWriter(int id)
+        {
+            var writerValue = writerManager.GetByID(id);
+            return View(writerValue);
+        }
+
+        [HttpPost]
+        public ActionResult EditWriter(Writer w)
+        {
+            ValidationResult results = writerValidator.Validate(w);
+            if (results.IsValid)
+            {
+                writerManager.WriterUpdate(w);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+
+            writerManager.WriterUpdate(w);
+            return View();
+
         }
     }
 }
